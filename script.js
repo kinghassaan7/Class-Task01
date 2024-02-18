32dda1aaebf3095f9a26b2c8894916d736a9992e
kinghassaan7 <145790867+kinghassaan7@users.noreply.github.com>
2024-02-18 08:14:44 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+// Welcome Message:
+// Create a function called displayWelcomeMessage that prompts the user to enter their name and then displays a personalized welcome message such as "Welcome, [name]!" in an alert.
+// function welcomeUser(name){
+//     alert('Hello ' + name)
+// }
+
+// welcomeUser("Hassaan")
+// welcomeUser("Mehtab")
+
+
+// ----------------------------------------------------------------------------------------------
+
+// Quote Generator: 
+// Create a function called generateQuote that randomly selects and returns a quote from an array of predefined quotes. Call this function to display a random quote on the webpage
+
+// function quoteGenerator(quotes) {
+//     let randomQuote = quotes[Math.floor(Math.random() * 3)]
+//     alert(randomQuote)
+// }
+
+// quoteGenerator(["Every Action has a reaction but Opposite in direction.", "A ship is safe at the Harbour, But thats not what Ships are made for.", "You only lose when you give up."])
+
+// ----------------------------------------------------------------------------------------------
+
+// Random Number Generator: 
+// Write a function called generateRandomNumber that generates and returns a random number between 1 and 100. Call this function and display the generated random number on the webpage.
+
+//  function getRandomNumber() {
+//     let randomNumber = Math.ceil(Math.random() * 100);
+//     alert(randomNumber)
+// }
+
+// getRandomNumber()
+
+
+
+
+
+
+
+
+
+
+
+
+
